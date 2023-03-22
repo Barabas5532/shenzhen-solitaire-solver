413125831113b7b210a1a4df072c4d97dd3745ed
Barabas Raffai <barabas.raffai@gmail.com>
2023-03-22 20:45:53 +0000

    Set up PyO3

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,5 @@
+use pyo3::prelude::*;
+
 mod card;
 mod game;
 mod game_state;
@@ -5,3 +7,16 @@ mod game_state;
 pub use card::*;
 pub use game::*;
 pub use game_state::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn maturin_test_project(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
